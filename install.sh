ba0d515d30cc7cf507a815978c8f770ac0dd8e41
Paul Nunnerley <mail@paulnunnerley.com>
2018-12-09 20:20:01 +0000

    Add additional submodules

--- install.sh
+++ install.sh
@@ -4,17 +4,14 @@ sudo apt install -y zsh build-essential git vim tmux
 git config --global user.name "Paul Nunnerley"
 git config --global user.email mail@paulnunnerley.com
 
-echo "Install ZSH and Oh My Zsh"
-rm -Rf ~/.oh-my-zsh 
-git clone git@github.com:robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-
-
 echo "Link dot files"
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/tmux.conf ~/.tmux.conf
 ln -s $PWD/vim ~/.vim
 ln -s $PWD/zshrc ~/.zshrc
-
+ln -s $PWD/oh-my-zsh ~/.oh-my-zsh
+ln -s $PWD/jenv ~/.jenv
+ln -s $PWD/nvm ~/.nvm
 
 echo "Download submodules"
 git submodule update --init

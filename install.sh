1e5d48966991fcd4d1b642b06dbb53dccb57e154
Paul Nunnerley <mail@paulnunnerley.com>
2018-12-09 18:13:33 +0000

    Install ohmzsh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo apt install -y zsh build-essential git vim tmux 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo chsh -S /bin/zsh
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/tmux.conf ~/.tmux.conf
 ln -s $PWD/vim ~/.vim
